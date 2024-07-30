5e487a60a83df0a5e69948e283d179913f8bcc7c
Kumbirai Muhwati <kmuhwati@mail.yu.edu>
2024-07-30 14:04:37 -0400

    Add files via upload

---
+++ apps/home.py
@@ -0,0 +1,31 @@
+# pip install openpyxl
+import pandas as pd
+import streamlit as st
+import zipfile
+import base64
+import os
+from PIL import Image
+from streamlit_lottie import st_lottie
+import json
+import requests
+
+
+
+
+
+def app():
+    st.title('Home')
+    st.markdown("""
+                ## Exploratory data analysis App By Tapiwa Chamboko.
+                
+
+
+
+---
+""")
+
+    st.write('')
+
+    st.write('**Welcome to Exploratory data analysis App**')
+    st.image('12.png', width=800)
+ 
